58d2eb3aee5f0fa9816d7ba43d87550e05cd5b7c
opknice <thanakrit_kas@hotmail.com>
2026-03-21 13:51:37 +0700

    TTT

--- proxy.cpp
+++ proxy.cpp
@@ -10,16 +10,20 @@
 typedef int (WSAAPI* send_t)(SOCKET s, const char* buf, int len, int flags);
 typedef int (WSAAPI* recv_t)(SOCKET s, char* buf, int len, int flags);
 typedef int (WSAAPI* connect_t)(SOCKET s, const struct sockaddr* name, int namelen);
+typedef int (WSAAPI* wsaconnect_t)(SOCKET s, const struct sockaddr* name, int namelen, LPWSABUF lpCallerData, LPWSABUF lpCalleeData, LPQOS lpSQOS, LPQOS lpGQOS);
+
+
 
 send_t pOriginalSend = NULL;
 recv_t pOriginalRecv = NULL;
 connect_t pOriginalConnect = NULL;
+wsaconnect_t pOriginalWSAConnect = NULL;
 
 // แก้ไขจาก BYTE เป็น BYTE[5] ทั้งหมด
 BYTE origSendBytes[5];
 BYTE origRecvBytes[5];
 BYTE origConnectBytes[5];
-
+BYTE origWSAConnectBytes[5];
 
 // --- Logger Function ---
 void WriteLog(const char* type, const char* buf, int len) {
@@ -105,6 +109,7 @@ int WSAAPI MyRecvHook(SOCKET s, char* buf, int len, int flags) {
     memcpy(pOriginalRecv, origRecvBytes, 5);
     int res = pOriginalRecv(s, buf, len, flags);
     
+    
     BYTE jmp[5] = { 0xE9 };
     *(DWORD*)(jmp + 1) = (DWORD)MyRecvHook - (DWORD)pOriginalRecv - 5;
     memcpy(pOriginalRecv, jmp, 5);
@@ -121,12 +126,35 @@ int WSAAPI MyRecvHook(SOCKET s, char* buf, int len, int flags) {
     return res;
 }
 
+int WSAAPI MyWSAConnectHook(SOCKET s, const struct sockaddr* name, int namelen, LPWSABUF lpCallerData, LPWSABUF lpCalleeData, LPQOS lpSQOS, LPQOS lpGQOS) {
+    struct sockaddr_in* addr = (struct sockaddr_in*)name;
+    unsigned short port = ntohs(addr->sin_port);
+
+    // เลี้ยวเฉพาะพอร์ตเกม (6900/6121) ไปหาบอท 6991
+    if (port == 6900 || port == 6121) {
+        addr->sin_addr.s_addr = inet_addr("127.0.0.1");
+        addr->sin_port = htons(6991);
+    }
+
+    DWORD old;
+    VirtualProtect(pOriginalWSAConnect, 5, PAGE_EXECUTE_READWRITE, &old);
+    memcpy(pOriginalWSAConnect, origWSAConnectBytes, 5);
+    int res = pOriginalWSAConnect(s, (const struct sockaddr*)addr, namelen, lpCallerData, lpCalleeData, lpSQOS, lpGQOS);
+    
+    BYTE jmp[5] = { 0xE9 };
+    *(DWORD*)(jmp + 1) = (DWORD)MyWSAConnectHook - (DWORD)pOriginalWSAConnect - 5;
+    memcpy(pOriginalWSAConnect, jmp, 5);
+    VirtualProtect(pOriginalWSAConnect, 5, old, &old);
+    return res;
+}
+
 // ส่วนที่ประกาศ Hooks ใน StartHooking ให้แก้เป็นแบบนี้จะชัวร์ที่สุดครับ
 void StartHooking() {
     HMODULE hWs2 = GetModuleHandleA("ws2_32.dll");
     pOriginalSend = (send_t)GetProcAddress(hWs2, "send");
     pOriginalRecv = (recv_t)GetProcAddress(hWs2, "recv");
     pOriginalConnect = (connect_t)GetProcAddress(hWs2, "connect");
+    pOriginalWSAConnect = (wsaconnect_t)GetProcAddress(hWs2, "WSAConnect");
 
     DWORD old;
 
@@ -158,6 +186,16 @@ void StartHooking() {
         *(DWORD*)(jmp + 1) = (DWORD)MyRecvHook - (DWORD)pOriginalRecv - 5;
         memcpy(pOriginalRecv, jmp, 5);
         VirtualProtect(pOriginalRecv, 5, old, &old);
+    }
+        // เพิ่มการติดตั้ง Hook WSAConnect
+    if (pOriginalWSAConnect) {
+        DWORD old;
+        VirtualProtect(pOriginalWSAConnect, 5, PAGE_EXECUTE_READWRITE, &old);
+        memcpy(origWSAConnectBytes, pOriginalWSAConnect, 5);
+        BYTE jmpW[5] = { 0xE9 };
+        *(DWORD*)(jmpW + 1) = (DWORD)MyWSAConnectHook - (DWORD)pOriginalWSAConnect - 5;
+        memcpy(pOriginalWSAConnect, jmpW, 5);
+        VirtualProtect(pOriginalWSAConnect, 5, old, &old);
     }
 }
 
